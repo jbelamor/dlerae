991a3ce144b6a47c71dc141b9ac766dfba15c32e
jbelamor <j.belena@alumnos.upm.es>
2019-11-25 14:07:14 +0100

    all the webpage but some php scripts

---
+++ init.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('select').material_select();
+    $('.scrollspy').scrollSpy();
+    $('.modal').modal();
+  });
+$('#definition').val('');
+$('#definition').trigger('autoresize');

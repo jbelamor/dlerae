802675c7ec97e429c1f5195fbb6ab4d6a64dd54e
jbelamor <j.belena@alumnos.upm.es>
2019-11-25 17:25:31 +0100

    typos fix + moar fixes + remove ads

--- init.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('select').material_select();
-    $('.scrollspy').scrollSpy();
-    $('.modal').modal();
-  });
-$('#definition').val('');
-$('#definition').trigger('autoresize');
